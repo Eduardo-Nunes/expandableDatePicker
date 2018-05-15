5d005a18aafff56110f28b88563111d5c9c51cb0
Eduardo Nunes <eduardo.nunes@ifactory.com.br>
2018-05-15 11:22:06 -0300

    Fist Commit

---
+++ app/src/main/java/com/nunes/eduardo/expandabledatepicker/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.nunes.eduardo.expandabledatepicker
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initViews()
+    }
+
+    private fun initViews() {
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+    }
+}
