a976c2644f78ebcc3e5890ffb730925d1d1816fd
Eduardo Nunes <eduardo.nunes@ifactory.com.br>
2018-05-16 11:19:43 -0300

    gradle organize version added Joda Time for android

--- app/src/main/java/com/nunes/eduardo/expandabledatepicker/ToolbarDatePicker.kt
+++ app/src/main/java/com/nunes/eduardo/expandabledatepicker/ToolbarDatePicker.kt
@@ -6,7 +6,6 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.util.AttributeSet
 import kotlinx.android.synthetic.main.date_toolbar_layout.view.*
-import java.util.*
 
 /**
  * TODO: document your custom view class.
