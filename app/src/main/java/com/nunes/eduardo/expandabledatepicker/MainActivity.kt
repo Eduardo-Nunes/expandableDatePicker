69b52fdbadda178ffdbad28461c988b23a406dbe
Eduardo Nunes <eduardo.nunes@ifactory.com.br>
2018-05-16 11:48:21 -0300

    getting formatted date string ans setting into title

--- app/src/main/java/com/nunes/eduardo/expandabledatepicker/MainActivity.kt
+++ app/src/main/java/com/nunes/eduardo/expandabledatepicker/MainActivity.kt
@@ -3,11 +3,14 @@ package com.nunes.eduardo.expandabledatepicker
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import net.danlew.android.joda.JodaTimeAndroid
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        JodaTimeAndroid.init(this)
+
         initViews()
     }
 
--- app/src/main/java/com/nunes/eduardo/expandabledatepicker/ToolbarDatePicker.kt
+++ app/src/main/java/com/nunes/eduardo/expandabledatepicker/ToolbarDatePicker.kt
@@ -6,14 +6,19 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.util.AttributeSet
 import kotlinx.android.synthetic.main.date_toolbar_layout.view.*
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
+import java.util.*
 
 /**
  * TODO: document your custom view class.
  */
+const val HUMAN_DATE_PATTERN: String = "dd MMMMM"
 class ToolbarDatePicker : Toolbar {
 
     private var _backgroundColor: Int = 0
     private var _contentColor: Int = 0
+    private lateinit var date: DateTime
 
     constructor(context: Context) : super(context) {
         init(null, 0)
@@ -52,7 +57,8 @@ class ToolbarDatePicker : Toolbar {
     }
 
     private fun initData() {
-//        TODO(date logic)
+        date = DateTime.now()
+        dateTitle.text = retrieveHumanDate(date)
     }
 
     private fun initListeners() {
@@ -70,6 +76,18 @@ class ToolbarDatePicker : Toolbar {
         dateTitle.setTextColor(_contentColor)
         expandCollapseDate.setColorFilter(_contentColor)
     }
+
+    private fun retrieveHumanDate(date: DateTime): String {
+        val dateString = DateTimeFormat
+                .forPattern(HUMAN_DATE_PATTERN)
+                .withLocale(Locale("pt", "BR"))
+                .print(date)
+                .toUpperCase()
+                .split(' ')
+
+        return context.getString(R.string.concateneted_date_text, dateString[0], dateString[1])
+    }
+
 
     fun setHomeAsUp(activity: AppCompatActivity){
         homeButton.setColorFilter(_contentColor)
