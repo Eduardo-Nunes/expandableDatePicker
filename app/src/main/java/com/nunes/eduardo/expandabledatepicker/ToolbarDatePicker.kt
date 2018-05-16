fe7ed350a2e9ffeaedffbca674984c29d4174bab
Eduardo Nunes <eduardo.nunes@ifactory.com.br>
2018-05-16 17:24:56 -0300

    week days list week days adapter week day item listener commented native date Picker

--- app/src/main/java/com/nunes/eduardo/expandabledatepicker/ToolbarDatePicker.kt
+++ app/src/main/java/com/nunes/eduardo/expandabledatepicker/ToolbarDatePicker.kt
@@ -2,7 +2,6 @@ package com.nunes.eduardo.expandabledatepicker
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.os.Build
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
@@ -74,8 +73,10 @@ class ToolbarDatePicker : Toolbar {
         _date = DateTime.now()
         dateTitle.text = retrieveHumanDate(_date)
 
-        datePicker.maxDate = _date.plusDays(_dateRange).millis
-        datePicker.minDate = _date.minusDays(_dateRange).millis
+//        datePicker.maxDate = _date.plusDays(_dateRange).millis
+//        datePicker.minDate = _date.minusDays(_dateRange).millis
+
+        weekDaysList.adapter = WeekDaysListAdapter(prepareDates(_date, _dateRange), { setDateClicked(it) })
     }
 
     private fun initListeners() {
@@ -88,16 +89,16 @@ class ToolbarDatePicker : Toolbar {
             expandableDatePicker.toggle(expandCollapseDate)
         }
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            datePicker.setOnDateChangedListener { _, year, monthOfYear, dayOfMonth ->
-                setDateClicked(DateTime().withDate(year, monthOfYear, dayOfMonth))
-            }
-        }else{
-            datePicker.init(_date.year, _date.monthOfYear, _date.dayOfMonth,
-                    { _, year, monthOfYear, dayOfMonth ->
-                        setDateClicked(DateTime().withDate(year, monthOfYear, dayOfMonth))
-                    })
-        }
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            datePicker.setOnDateChangedListener { _, year, monthOfYear, dayOfMonth ->
+//                setDateClicked(DateTime().withDate(year, monthOfYear, dayOfMonth))
+//            }
+//        }else{
+//            datePicker.init(_date.year, _date.monthOfYear, _date.dayOfMonth,
+//                    { _, year, monthOfYear, dayOfMonth ->
+//                        setDateClicked(DateTime().withDate(year, monthOfYear, dayOfMonth))
+//                    })
+//        }
     }
 
     private fun setDateClicked(date: DateTime){
@@ -123,6 +124,15 @@ class ToolbarDatePicker : Toolbar {
         return context.getString(R.string.concateneted_date_text, dateString[0], dateString[1])
     }
 
+    private fun prepareDates(initialDate: DateTime, rangeDays: Int): ArrayList<DateTime> {
+        val dateList: ArrayList<DateTime> = arrayListOf()
+
+        for (i in (rangeDays * -1)..rangeDays) {
+            dateList.add(initialDate.plusDays(i))
+        }
+
+        return dateList
+    }
 
     fun setHomeAsUp(activity: AppCompatActivity){
         homeButton.setColorFilter(_contentColor)
